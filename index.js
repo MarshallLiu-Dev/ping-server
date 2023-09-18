c6c35d34265d3c0cbbc6ae5dfd69668ca3c8d464
MarshallLiu-Dev <paulomartins12358@gmail.com>
2023-09-18 10:29:54 -0300

    alone

--- index.js
+++ index.js
@@ -1,38 +1,30 @@
 require('dotenv').config();
 
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 const cors = require('cors');
-const mongoose = require("mongoose");
-const User = require("./models/User");
-const Post = require('./models/Post');
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const multer = require('multer');
-const uploadMiddleware = multer({ dest: 'uploads/' });
+const app = express();
 const fs = require('fs');
 const path = require('path');
-const secret = 'asdfe45we45w345wegw345werjktjwertkj';
-
-
-app.use(
-    cors({
-        origin: ['https://jovial-maamoul-38ed7a.netlify.app','https://pingsocial.vercel.app', 'http://192.168.0.112:3000', 'http://localhost:3000'],
-        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-        credentials: true, 
-    })
-);
-
-app.use(express.json());
-app.use(cookieParser());
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
+const secret = 'asdfe45we45w345wegw345werjktjwertkj';
+const cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
+app.use(express.json());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+// Configurar o CORS
+app.use(cors({ origin: '*', credentials: true }));
+
+// Configurar o cookie-parser
+app.use(cookieParser());
 
 // Conexão com o MongoDB
-
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -45,8 +37,6 @@ mongoose.connect(process.env.MONGODB_URI, {
     });
 
 // Rota para registro de usuário
-
-//test
 app.post('/register', uploadMiddleware.single('file'), async (req, res) => {
     const { username, password, name, email, phone, age, bio } = req.body;
 
@@ -75,35 +65,7 @@ app.post('/register', uploadMiddleware.single('file'), async (req, res) => {
     }
 });
 
-// // Rota para login de usuário
-// app.post('/login', async (req, res) => {
-//     const { username, password } = req.body;
-//     const userDoc = await User.findOne({ username });
-
-//     if (!userDoc) {
-//         return res.status(400).json("Falha ao realizar o Login");
-//     }
-
-//     const passOk = bcrypt.compareSync(password, userDoc.password);
-
-//     if (passOk) {
-//         jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
-//             if (err) throw err;
-
-//             const responseJSON = {
-//                 id: userDoc._id,
-//                 username,
-//                 profileImage: userDoc.profileImage,
-//             };
-
-//             res.cookie('token', token).json(responseJSON);
-//         });
-//     } else {
-//         res.status(400).json("Falha ao realizar o Login");
-//     }
-// });
-
-// Rota para login de usuário
+//Rota para login de usuário
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
     const userDoc = await User.findOne({ username });
@@ -115,7 +77,7 @@ app.post('/login', async (req, res) => {
     const passOk = bcrypt.compareSync(password, userDoc.password);
 
     if (passOk) {
-        jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+        jwt.sign({ username, id: userDoc._id }, secret, { expiresIn: '1h' }, (err, token) => {
             if (err) throw err;
 
             const responseJSON = {
@@ -124,26 +86,24 @@ app.post('/login', async (req, res) => {
                 profileImage: userDoc.profileImage,
             };
 
-            // Adicione um log para verificar o responseJSON
+            //log para verificar o responseJSON
             console.log('responseJSON:', responseJSON);
 
-            res.cookie('token', token).json(responseJSON);
+            res.cookie('token', token, {
+                domain: 'https://pingsocial.vercel.app',
+                path: '/',
+                httpOnly: true,
+                secure: true, 
+                maxAge: 1296000000,
+              }).json(responseJSON);
+
         });
     } else {
         res.status(400).json("Falha ao realizar o Login");
     }
 });
 
-
 // Rota para perfil de usuário
-app.get('/profile', (req, res) => {
-    const { token } = req.cookies;
-    jwt.verify(token, secret, {}, (err, info) => {
-        if (err) throw err;
-        res.json(info);
-    });
-});
-
 app.get("/profile", (req, res) => {
     const { token } = req.cookies;
 
@@ -215,7 +175,7 @@ app.get('/', async (req, res) => {
         const posts = await Post.find()
             .populate('author', ['username', 'name'])
             .sort({ createdAt: -1 })
-            .limit(20);
+            .limit(20000);
 
         res.json(posts);
     } catch (error) {
@@ -224,7 +184,7 @@ app.get('/', async (req, res) => {
     }
 });
 
-// Rota para buscar o perfil do usuário
+// Rota para buscar o perfil do usuário com posts ordenados por data decrescente
 app.get('/perfil', async (req, res) => {
     try {
         const { token } = req.cookies;
@@ -235,7 +195,10 @@ app.get('/perfil', async (req, res) => {
             return res.status(404).json({ error: 'Usuário não encontrado' });
         }
 
-        const userPosts = await Post.find({ author: user._id });
+        // Consulta os posts do usuário, ordenando por data decrescente
+        const userPosts = await Post.find({ author: user._id })
+            // .sort({ createdAt: -1 }); // Ordena por data decrescente
+            .sort({ createdAt: 1 }) // Ordena por data crescente
 
         res.json({
             username: user.username,
@@ -250,6 +213,7 @@ app.get('/perfil', async (req, res) => {
         res.status(500).json({ error: 'Erro ao buscar o perfil do usuário' });
     }
 });
+
 
 // Rota para atualizar o perfil do usuário
 app.put('/profile', async (req, res) => {
@@ -274,35 +238,29 @@ app.put('/profile', async (req, res) => {
     }
 });
 
-// Rota para a edição de postagens (HTTP PUT)
 app.get("/edit-post/:postId",  async (req, res) => {
     const { postId } = req.params;
 
     try {
-        // Consulte o banco de dados para obter os detalhes da postagem com base no postId
+
         const post = await Post.findById(postId);
 
         if (!post) {
-            return res.status(404).json({ error: "Post not found" });
+            return res.status(404).json({ error: "Post não encontrado" });
         }
 
-        // Verifique se o usuário autenticado é o autor da postagem
+
         if (post.author.toString() !== req.user.id) {
-            return res.status(403).json({ error: "Unauthorized" });
+            return res.status(403).json({ error: "não autorizado" });
         }
 
-        // Atualize os campos da postagem com base nos dados recebidos na solicitação PUT
         post.summary = req.body.summary;
-        // Atualize outros campos, se necessário
-
-        // Salve a postagem atualizada no banco de dados
         await post.save();
 
-        // Envie a postagem atualizada como resposta
         res.json(post);
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: "Error updating post" });
+        res.status(500).json({ error: "Error ao atualizar os post" });
     }
 });
 
@@ -314,8 +272,7 @@ app.get('/post/:id', async (req, res) => {
     res.json(postDoc);
 });
    
-
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Servidor rodando na porta ${port}`);
 });
